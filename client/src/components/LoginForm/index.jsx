b8866f1371f53e6458d2fb03f9a747ae6ccec0b8
Wayne <wayne.c.griffiths@gmail.com>
2020-08-27 19:26:42 +0800

    UPDATE client package.json as client dependencies were installed in the server package.json

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Form } from 'grass-roots-react';
 
 export default function(){
 
-    
+    return (
+        // login form
+        <form>
+            
+        </form>
+    )
+
 }
--- client/src/pages/Signin/index.jsx
+++ client/src/pages/Signin/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loginfrom from '../../components/LoginForm';
 
 export default function Signin(){
     return (
