b7533e084f9a4e8c3ff2583958ef86e6ea80ac3f
Wayne <wayne.c.griffiths@gmail.com>
2020-08-27 17:46:55 +0800

    ADD import statements to App.js to actually import the react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Signin from './pages/Signin';
 import NotFound from './pages/NotFound';
