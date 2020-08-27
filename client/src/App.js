4603c415464620b98e01df4537e08416ffc0864d
Wayne <wayne.c.griffiths@gmail.com>
2020-08-27 22:24:52 +0800

    Remove backgroundImageRouter from hooks as this is being served from grass-roots-react properly now

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Dashboard from './pages/Dashboard';
 import Signin from './pages/Signin';
 import NotFound from './pages/NotFound';
 import Header from './components/Header';
-import useBackgroundImageRouter from './hooks/useBackgroundImageRouter';
+import { useBackgroundImageRouter } from 'grass-roots-react';
 import { backgroundImageMap } from './content'
 
 function App() {
