1dc7c49d27b5b10b1b4a582d1164825acda8eb51
Wayne <wayne.c.griffiths@gmail.com>
2020-08-27 18:43:43 +0800

    ADD useBackgroundImageRouter from grass-roots-react. Created a imageMap in content that maps background images to router endpoints

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,13 @@ import Dashboard from './pages/Dashboard';
 import Signin from './pages/Signin';
 import NotFound from './pages/NotFound';
 import Header from './components/Header';
+import { useBackgroundImageRouter } from 'grass-roots-react';
+import { backgroundImageMap } from './content'
 
 function App() {
+
+  useBackgroundImageRouter(backgroundImageMap);
+
   return (
     <Router>
           <Header
---
+++ client/src/content/index.js
@@ -0,0 +1,8 @@
+import typewriter from './images/backgroundImages/typewriter.jpg'
+import table from './images/backgroundImages/table.jpg'
+
+export const backgroundImageMap = {
+    '/':{ image: table, positionX:'center', positionY:'10vh'},
+    '/dashboard':{ image: table, positionX:'center', positionY:'10vh'},
+    '/signin':{ image: typewriter, positionX:'center', positionY:'10vh'},
+}
