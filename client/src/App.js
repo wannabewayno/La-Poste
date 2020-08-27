4165720557f198e2ee590cbfd903c271460ae030
Wayne <wayne.c.griffiths@gmail.com>
2020-08-27 17:02:35 +0800

    ADD react router and pages dir with Dashboard and Signin both rendering a simple p-tag that states what page it is

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Dashboard from './pages/Dashboard';
+import Signin from './pages/Signin';
 
 function App() {
   return (
-    <>
-      <div> Hello world </div>
-    </>
+    <Router>
+          <Header
+            text='La Poste'
+            color='white'
+          />
+          <Switch>
+            <Route exact path={["/", "/dashboard"]}>
+              <Dashboard/>
+            </Route>
+            <Route exact path='/signin'>
+              <Signin/>
+            </Route>
+            <Route>
+              <NotFound/>
+            </Route>
+          </Switch>
+    </Router>
   )
 }
 
---
+++ client/src/pages/Signin/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function (){
+    return (
+        <p>this is a signin page</p>
+    )
+}
