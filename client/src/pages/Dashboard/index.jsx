26e14a71f2bfdbe54402ce496320ed5f9c10fbc1
Wayne <wayne.c.griffiths@gmail.com>
2020-08-28 08:38:47 +0800

    UPDATE Post, Footer, MenuButtons and SideBar to return a trivial p tag

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import SideBar from '../../components/SideBar';
+import Footer from '../../components/Footer';
+import MenuButtons from '../../components/MenuButtons';
+import Post from '../../components/Post';
+import { Container } from 'grass-roots-react';
 
 export default function Dashboard(){
     return (
-        <p>this is a dashboard</p>
+        <>
+            <SideBar/>
+
+            <Container>
+                <Post/>
+            </Container>
+
+            <Footer/>
+        </>
     )
 }
