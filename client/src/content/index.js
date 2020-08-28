c6b9b0bcd0463e1ac21580bd67f7e92d283a7a89
Wayne <wayne.c.griffiths@gmail.com>
2020-08-28 17:12:12 +0800

    change the backgroundImageMap to reflect the new root route, routing to the signin page

--- client/src/content/index.js
+++ client/src/content/index.js
@@ -2,7 +2,7 @@ import typewriter from './images/backgroundImages/typewriter.jpg'
 import table from './images/backgroundImages/table.jpg'
 
 export const backgroundImageMap = {
-    '/':{ image: table, positionX:'center', positionY:'10vh'},
+    '/':{ image: typewriter, positionX:'center', positionY:'10vh'},
     '/dashboard':{ image: table, positionX:'center', positionY:'10vh'},
     '/signin':{ image: typewriter, positionX:'center', positionY:'10vh' },
 }
