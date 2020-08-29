b19c5cbe685e8e85aef4eaf9f170791d3c107699
Wayne <wayne.c.griffiths@gmail.com>
2020-08-29 10:53:38 +0800

    ADD css markup for hovering over titles in the sidebar list

--- client/src/components/PostList/index.jsx
+++ client/src/components/PostList/index.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { updateCurrentPost } from '../../redux/actions';
 import { useDispatch } from 'react-redux';
+import { onHover } from 'grass-roots-react';
 
 export default function({ posts = [] }){
 
     const dispatch = useDispatch();
 
-    function clickPost(postId){
-        console.log('clickPost');
-        dispatch(updateCurrentPost(postId));    
-    }
-    console.log('PostList:',posts);
+    // call this when clicking on a title from the sidebar list, updates the current post
+    const clickPost = (postId) => dispatch(updateCurrentPost(postId));
+
+    const hoverStyle = { color:'rgb(255,125,125)', cursor:'pointer' }
+    
     return (
         <ul>
             {posts.map(({ title, id },index) => (
-                    <li key={index} onClick={() => clickPost(id)}>
+                    <li
+                        key = {index}
+                        onClick = {() => clickPost(id)}
+                        {...onHover(hoverStyle)}
+                    >
                         {title}
                     </li>
                 )
