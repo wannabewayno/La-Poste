817981166aac9ea6e744f11c77af2e5a9017bc54
Wayne <wayne.c.griffiths@gmail.com>
2020-08-29 16:35:17 +0800

    ADD isFetchingPosts to reducer pool

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import posts from './posts';
 import modal from './modal';
 import isLoggedIn from './isLoggedIn';
+import isFetchingPosts from './isFetchingPosts';
 
 export default combineReducers({
     posts,
     modal,
     isLoggedIn,
+    isFetchingPosts,
 })
